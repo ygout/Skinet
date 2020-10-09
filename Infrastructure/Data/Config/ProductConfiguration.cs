fd009784456aafc9f420c40e2f2612d0ba364511
Yohann <yohann.goutaret@gfi.fr>
2020-10-09 12:06:40 +0200

    repo

--- Infrastructure/Data/Config/ProductConfiguration.cs
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -10,7 +10,10 @@ namespace Infrastructure.Data.Config
         {
             builder.Property(p => p.Id).IsRequired();
             builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
-            builder.Property(p => p.Description).IsRequired().HasMaxLength(255)
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(180);
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2");
+            builder.HasOne(b => b.ProductBrand).WithMany().HasForeignKey(p => p.ProductBrandId);
+            builder.HasOne(t => t.ProductType).WithMany().HasForeignKey(t => t.ProductTypeId);
         }
     }
 }
